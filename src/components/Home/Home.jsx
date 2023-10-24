09f96276cabed2e6a4bed57848adc5bb989d17b2
Abdullah Al Mamun <abdullaalmamun100@gmail.com>
2023-10-24 17:14:43 +0800

    Footer update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,3 +1,31 @@
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <header className="fixed-top">
+        <div>
+          <div>
+            <div>
+              <ul className="flex justify-center gap-20">
+                <li>Sales</li>
+                <li>Campaigns</li>
+                <li>logo</li>
+                <li>Screens</li>
+                <li>Networks</li>
+                <li>Sales</li>
+              </ul>
+            </div>
+            <div></div>
+          </div>
+        </div>
+      </header>
+      <section>
+        <div>
+          <img
+            src="https://media.geeksforgeeks.org/wp-content/uploads/20221105184949/ezgif.com-gif-maker.mp4"
+            alt=""
+          />
+        </div>
+      </section>
+    </>
+  );
 };
