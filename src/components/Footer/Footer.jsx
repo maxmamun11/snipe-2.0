263187f4f1652e5c938c67e55549ee8c8ec18f0a
Abdullah Al Mamun <abdullaalmamun100@gmail.com>
2023-10-25 12:23:33 +0800

    update footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,51 +2,50 @@ import snipeLogo from "../../assets/icon.png";
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-700">
-      <div className="w-full max-w-screen-xl p-4 py-6 mx-auto c lg:py-8 md:container">
-        <div className="md:flex md:justify-between">
-          <div className="grid grid-cols-3 gap-8 text-white sm:gap-6 sm:grid-cols-3">
-            <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-200 uppercase">
-                Contact
-              </h2>
-              <p>
-                Tractive Sdn Bhd <br /> Mobile: +60192333656 <br /> Email:
-                info@snipeads.com{" "}
-              </p>
-              <h2 className="mb-6 text-sm font-semibold text-gray-200 uppercase">
-                Hours
-              </h2>
-              <p>Monday - Friday: 8am - 5pm</p>
-                <div>
-                <p>Monday - Friday: 8am - 5pm</p>
-                </div>
-              
-              
-            </div>
-            <div className="text-white">
-              <h2 className="mb-6 text-sm font-semibold uppercase ">Address</h2>
-              <p>
-                A-2-28, jalan PJu 1/43, Aman Suria, 47301 <br /> Petaling Jaya,
-                Selangor, Malaysia
-              </p>
-            </div>
-            <div className="">
-              <img src={snipeLogo} className="mr-1 h-1/2" alt="logo" />
-            </div>
-          </div>
+    <footer className="py-6 text-white bg-gray-900">
+      <div className="container flex flex-wrap mx-auto">
+      {/* The  */}
+        <div className="w-full mb-4 sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3">
+          <h2 className="mb-2 text-lg font-bold">Contact</h2>
+          <p>
+            Tractive Sdn Bhd <br /> Mobile: +60192333656 <br /> Email:
+            info@snipeads.com{" "}
+          </p>
+
+          <h2>Operation Hours</h2>
+          <p>Monday - Friday : 8AM - 5PM</p>
+          {/* Privacy policy need to link with privacy  */}
+          <h1>Privacy Policy</h1>
+          <h1>Terms and Conditions</h1>
         </div>
-        <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
-        <div className="sm:flex sm:items-center sm:justify-between">
-          <span className="text-sm text-gray-500 sm:text-center">
-            © 2023 {""}
-            <a href="https://tractive.com.my/" className="hover:underline">
-              {""}
-              snipeads
-            </a>
-            . All Rights Reserved.
-          </span>
+
+        <div className="w-full mb-4 sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3">
+          <h1>Address</h1>
+          <p>
+            A-2-28, jalan PJu 1/43, Aman Suria, 47301 <br /> Petaling Jaya,
+            Selangor, Malaysia
+          </p>
         </div>
+        <div className="w-full mb-4 sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3">
+          {/* <h2>Snipeads</h2> */}
+          <img
+            src={snipeLogo}
+            alt="logo"
+            className="bottom-0 right-0 object-position:"
+          />
+        </div>
+      </div>
+
+      <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+      <div className="sm:flex sm:items-center sm:justify-between">
+        <span className="text-sm text-gray-500 sm:text-center">
+          © 2023 {""}
+          <a href="https://tractive.com.my/" className="hover:underline">
+            {""}
+            snipeads
+          </a>
+          . All Rights Reserved.
+        </span>
       </div>
     </footer>
   );
